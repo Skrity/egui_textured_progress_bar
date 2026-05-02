1318be003d060ec00fe7177bc5f818e8d9600e8e
Skrity <skrityx@gmail.com>
2026-05-02 09:12:19 +0300

    support egui 0.34

--- examples/progress_bars/main.rs
+++ examples/progress_bars/main.rs
@@ -10,7 +10,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "Confirm exit",
+        "Progress Bars",
         options,
         Box::new(|_cc| {
             _cc.egui_ctx.set_pixels_per_point(2.0);
@@ -25,27 +25,30 @@ struct MyApp {
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
             ui.add(egui::DragValue::new(&mut self.progress));
             let progress = self.progress / 100.0;
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
+                    .text("Basic")
                     .animate(true)
                     .fill(Color32::GREEN),
             );
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
+                    .text("With texture")
                     .animate(true)
-                    .corner_radius(5)
-                    .fill(Color32::LIGHT_RED)
-                    .texture(progress_bar_texture(ctx)),
+                    .fill(Color32::YELLOW)
+                    .texture(progress_bar_texture(ui)),
             );
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
+                    .text("With texture + custom corners")
                     .animate(true)
-                    .fill(Color32::YELLOW)
-                    .texture(progress_bar_texture(ctx)),
+                    .corner_radius(5)
+                    .fill(Color32::LIGHT_RED)
+                    .texture(progress_bar_texture(ui)),
             );
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
@@ -53,32 +56,33 @@ impl eframe::App for MyApp {
                     .fill(Color32::LIGHT_YELLOW)
                     .text("XDD")
                     .show_percentage()
-                    .texture(progress_bar_texture(ctx)),
+                    .texture(progress_bar_texture(ui)),
             );
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
+                    .text("Different")
                     .animate(true)
                     .fill(Color32::LIGHT_GREEN)
-                    .text("XDD")
                     .corner_radius(7)
-                    .texture(progress_bar_texture(ctx)),
+                    .texture(progress_bar_texture(ui)),
             );
             ui.add(
                 egui_textured_progress_bar::ProgressBar::new(progress)
+                    .text("Non animated")
                     .animate(false)
                     .fill(Color32::LIGHT_BLUE)
                     .text("XDD")
                     .corner_radius(7)
-                    .texture(progress_bar_texture(ctx)),
+                    .texture(progress_bar_texture(ui)),
             );
         });
     }
 }
 /// Texture for progress bar
-fn progress_bar_texture(ctx: &eframe::egui::Context) -> &eframe::egui::TextureHandle {
+fn progress_bar_texture(ui: &eframe::egui::Ui) -> &eframe::egui::TextureHandle {
     static TEXTURE: std::sync::OnceLock<eframe::egui::TextureHandle> = std::sync::OnceLock::new();
     TEXTURE.get_or_init(|| {
-        ctx.load_texture(
+        ui.load_texture(
             "progress_bar_texture",
             eframe::egui::ColorImage::from_rgba_unmultiplied(
                 [20, 20],
